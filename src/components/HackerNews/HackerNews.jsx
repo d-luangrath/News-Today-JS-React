a696410ddd120ec8700946b90ca8398327a4e579
Darla Luangrath <darla.luangrath@gmail.com>
2024-09-24 14:53:17 -0700

    Set up Redux and connect to React app, HTTP req to an API, display articles

--- src/components/HackerNews/HackerNews.jsx
+++ src/components/HackerNews/HackerNews.jsx
@@ -1,10 +1,17 @@
-import { useState } from 'react';
+import { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { requestArtciles } from '../../reducers/hackerNewsReducer.js';
 import Card from '../shared/Card/Card.jsx';
 import Loading from '../shared/Loading/Loading.jsx';
 
 export default function HackerNews() {
-  const [articles, setArticles] = useState([]);
-  const [loading, setLoading] = useState(true);
+    const loading = useSelector((state) => state.HackerNews.loading);
+    const articles = useSelector((state) => state.HackerNews.articles);
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+      dispatch(requestArtciles);
+    }, [])
 
   const articleCards = articles.map((article) => <Card key={article.id} article={article} />);
   return (
---
+++ src/reducers/hackerNewsReducer.js
@@ -0,0 +1,27 @@
+import axios from "axios";
+
+const initialState = {
+    loading: false,
+    articles: []
+};
+
+const PENDING = 'PENDING';
+const REQUEST_ARTICLES = 'REQUEST_ARTICLES';
+
+export const requestArtciles = async(dispatch) => {
+    dispatch({type: PENDING})
+    let articles = await axios.get('/api/hacker-news').then(res => res.data);
+    dispatch({type: REQUEST_ARTICLES, payload: articles})
+}
+
+export default function hackerNewsReducer (state=initialState, action) {
+    switch(action.type) {
+        case PENDING:
+            return { ...state, loading: true };
+        case REQUEST_ARTICLES:
+            return { loading: false, articles: action.payload };
+    default:
+        return state;
+    }
+}
+
